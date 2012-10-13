2232ac768f5536eb8db2166c59cee12172828b9a
Dmitry Maksimov <dmtmax@gmail.com>
2012-10-13 07:52:17 +0400

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Potee::Application.config.secret_token = '6c9ce6c2df6941951751c7d48db46bc8b78e047b5a707cbe507831e4527bbc3ecd5246aea0f716b384f22869eff4b9fe010414689c0442bec4f74e70c00a4596'
