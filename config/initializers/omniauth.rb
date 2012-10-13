6ea8fa84590922f0dac7b833418b050a4079924b
Dmitry Maksimov <dmtmax@gmail.com>
2012-10-13 10:01:06 +0400

    Added authentication via Twitter

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, Settings.facebook.key, Settings.facebook.secret, scope: 'email'
-#   provider :twitter, 'aIge6ADDxeRLL0D0DfNiA', 'WZr5DPDmLNJK1JGEaStArnCki0Rau8yS3mZInpBSh0'
+  provider :twitter, Settings.twitter.key, Settings.twitter.secret
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Potee::Application.routes.draw do
   match '/auth/:provider/callback', to: 'sessions#create'
+  match '/auth/failure', to: redirect('/')
   match 'logout', to: 'sessions#destroy', as: 'logout'
 
   root to: 'welcome#index'
