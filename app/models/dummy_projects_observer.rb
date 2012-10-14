fc24b0b32d2fa18dbaedc55d0bbd93ec576a5c23
Anton Kalyaev <anton.kalyaev@gmail.com>
2012-10-15 01:29:55 +0400

    added dummy data observer

---
+++ app/models/dummy_projects_observer.rb
@@ -0,0 +1,26 @@
+class DummyProjectsObserver < ActiveRecord::Observer
+  observe :user
+
+  def after_create(user)
+    projects = user.projects.create([
+      {title: 'Learn Scala', started_at: Date.today, finish_at: 10.days.since, color_index: 1},
+      {title: 'Make my wife happy', started_at: Date.today, finish_at: 11.days.since, color_index: 2},
+      {title: 'Start my own business', started_at: Date.today, finish_at: 12.days.since, color_index: 3},
+    ])
+    p = projects.first
+    p.events.create([
+      {title: 'By a book', date: Date.tomorrow, time: Time.current},
+      {title: 'Read some interesting posts', date: 4.days.since, time: Time.current},
+      {title: 'Went to the conference', date: 7.days.since, time: Time.current}
+    ])
+    p = projects[1]
+    p.events.create([
+      {title: 'Buy a present', date: Date.tomorrow, time: Time.current},
+      {title: 'Go shopping together', date: 10.days.since, time: Time.current}
+    ])
+    p = projects.last
+    p.events.create([
+      {title: 'Think about the idea', date: Date.tomorrow, time: Time.current}
+    ])
+  end
+end
