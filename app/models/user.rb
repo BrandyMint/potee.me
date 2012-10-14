e8c5295eb749b58425d1aa62f7f81c138f93f359
Anton Kalyaev <anton.kalyaev@gmail.com>
2012-10-15 01:04:33 +0400

    added dependent:destroy attrs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   attr_protected :secret
 
-  has_many :authentications
-  has_many :projects
+  has_many :authentications, :dependent => :destroy
+  has_many :projects, :dependent => :destroy
 
   def to_s
     name || email
