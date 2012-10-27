5d7390a5cafe04dda6cd6d098d9c56494c59c9a9
Alex Naumov <alexsnaumov@gmail.com>
2012-10-27 16:51:01 +0700

    [#37724329] аватар для пользователя

---
+++ app/uploaders/avatar_uploader.rb
@@ -0,0 +1,21 @@
+# encoding: utf-8
+
+class AvatarUploader < CarrierWave::Uploader::Base
+
+  include CarrierWave::MiniMagick
+
+  storage :file
+  
+  version :thumb do
+    process resize_to_fill: [35, 35]
+  end  
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def extension_white_list
+     %w(jpg jpeg gif png)
+  end
+
+end
