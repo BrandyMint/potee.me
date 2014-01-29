4ce11490ee24861a063f969e591a91677eeae42c
Danil Pismenny <danil@orionet.ru>
2014-01-29 17:43:33 +0400

    Show version in admin dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -5,8 +5,8 @@ ActiveAdmin.register_page "Dashboard" do
   content :title => proc{ I18n.t("active_admin.dashboard") } do
     div :class => "blank_slate_container", :id => "dashboard_default_message" do
       span :class => "blank_slate" do
-        span "Welcome to Active Admin. This is the default dashboard page."
-        small "To add dashboard sections, checkout 'app/admin/dashboards.rb'"
+        span "Version: #{Masha.version}"
+        #small "To add dashboard sections, checkout 'app/admin/dashboards.rb'"
       end
     end
 
