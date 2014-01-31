9153be36ef2b38e391568b4c2d19032385016c94
Danil Pismenny <danil@orionet.ru>
2014-01-31 17:33:19 +0400

    Отключил сидирование

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,42 +6,39 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-p1 = Project.create :title => 'Design for google.com',
-  :started_at => Date.parse('2012-09-01'),
-  :finish_at => Date.parse('2012-12-01'),
-  :color_index => 1
+#p1 = Project.create :title => 'Design for google.com',
+  #:started_at => Date.parse('2012-09-01'),
+  #:finish_at => Date.parse('2012-12-01')
 
-p1.events.create(
-  title: 'Prepare vision document',
-  date: Date.parse('2012-09-01'),
-  time: Time.parse('2012-09-01 12:00')
-)
+#p1.events.create(
+  #title: 'Prepare vision document',
+  #date: Date.parse('2012-09-01'),
+  #time: Time.parse('2012-09-01 12:00')
+#)
 
-p1.events.create(
-  title: 'Make some thoughts',
-  date: Date.parse('2012-10-25'),
-  time: Time.parse('2012-10-25 14:00')
-)
+#p1.events.create(
+  #title: 'Make some thoughts',
+  #date: Date.parse('2012-10-25'),
+  #time: Time.parse('2012-10-25 14:00')
+#)
 
-p2 = Project.create :title => 'Make wife happy',
-  :started_at => Date.parse('2012-09-15'),
-  :finish_at => Date.parse('2012-11-04'),
-  :color_index => 2
+#p2 = Project.create :title => 'Make wife happy',
+  #:started_at => Date.parse('2012-09-15'),
+  #:finish_at => Date.parse('2012-11-04')
 
-p2.events.create(
-  title: 'Buy tickets to Oper',
-  date: Date.parse('2012-10-03'),
-  time: Time.parse('2012-10-03 21:00')
-)
+#p2.events.create(
+  #title: 'Buy tickets to Oper',
+  #date: Date.parse('2012-10-03'),
+  #time: Time.parse('2012-10-03 21:00')
+#)
 
-p3 = Project.create :title => 'Learn SCALA',
-  :started_at => Date.parse('2012-09-25'),
-  :finish_at => Date.parse('2013-01-09'),
-  :color_index => 3
+#p3 = Project.create :title => 'Learn SCALA',
+  #:started_at => Date.parse('2012-09-25'),
+  #:finish_at => Date.parse('2013-01-09')
 
-p3.events.create(
-  title: 'Read the BOOK',
-  date: Date.parse('2012-11-12'),
-  time: Time.parse('2012-11-12 6:00')
-)
+#p3.events.create(
+  #title: 'Read the BOOK',
+  #date: Date.parse('2012-11-12'),
+  #time: Time.parse('2012-11-12 6:00')
+#)
 
